ffb0c66d3b3c878e68875816c9924e5ebcfc9361
Robin Choudhury <xrobinc@gmail.com>
2013-08-16 12:25:21 -0700

    Add quotes and force symlink.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ files=('vimrc' 'vim')
 git submodule init && git submodule update
 
 for i in "${files[@]}"; do
-   rm -r ~/.$i
-   ln -s "$PWD/$i" ~/."$i"
+   rm -r ~/."$i"
+   ln -sf "$PWD/$i" ~/."$i"
 done
