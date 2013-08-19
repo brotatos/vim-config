08cc24db2276cec53a46b3eb95e5ec9065395304
Robin Choudhury <xrobinc@gmail.com>
2013-08-19 13:14:59 -0700

    Update vundle on setup.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ files=('vimrc' 'vim')
 
 git submodule init && git submodule update
 
-cd vim/bundle/vundle; git pull origin master
+cd vim/bundle/vundle
+git pull origin master
+cd -
 
 for i in "${files[@]}"; do
    rm -r ~/."$i"
