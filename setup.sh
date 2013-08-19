4e06419e86575be84264e5fb75d2b6ef55c28bff
Robin Choudhury <xrobinc@gmail.com>
2013-08-19 13:11:35 -0700

    Update.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ files=('vimrc' 'vim')
 
 git submodule init && git submodule update
 
+cd vim/bundle/vundle; git pull origin master
+
 for i in "${files[@]}"; do
    rm -r ~/."$i"
    ln -sf "$PWD/$i" ~/."$i"
